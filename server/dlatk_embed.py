aeedac5803bf56ad1807e886d78820e95c98587f
ojasdeshpande10 <ojasdesh2000@gmail.com>
2025-02-24 11:58:38 -0500

    timings changes

--- server/dlatk_embed.py
+++ server/dlatk_embed.py
@@ -9,6 +9,7 @@ from dlatk.transformer_embs import transformer_embeddings
 import numpy as np
 from tqdm import tqdm
 import torch
+import time
 
 
 
@@ -16,9 +17,10 @@ class MessageEmbedder:
     def __init__(self, modelName):
         self.messages = []
         self.batch_size = 512
-        self.embedding_model = self.create_embeddinng_model(modelName)
+        self.embedding_model = self.create_embedding_model(modelName)
         
-    def create_embeddinng_model(self, modelName):
+    def create_embedding_model(self, modelName):
+        print("Creating embedding model")
         cf_embedding_generator = transformer_embeddings(
             modelName=modelName,
             tokenizerName=modelName,
@@ -34,8 +36,9 @@ class MessageEmbedder:
 
         return cf_embedding_generator
     def get_embeddings(self, messages):
-        
-        
+
+    # messages is a list of lists, each inner list has two elements, message_id and message
+    ############################################################################
         # Batching process
         groupedMessageRows = []
         batch_counter = 0
@@ -47,11 +50,13 @@ class MessageEmbedder:
             # messages are in list of list format, inside list has two elementes msg_id and message
             grouped_batch = [[msg_in[0], msg_in[1]] for msg_in in batch]
             groupedMessageRows.append(["dummy_" + str(batch_counter), grouped_batch])
-
         all_embeddings = []
+        timings = {"prepare_messages": 0, "model_call": 0, "padding":0, "layer_extraction":0, "message_aggregate": 0, "embedding_stacking" : 0}
         for batch in tqdm(groupedMessageRows):
+            start_time = time.time()
             tokenIdsDict, (cfId_seq, msgId_seq) = self.embedding_model.prepare_messages([batch], sent_tok_onthefly=True, noContext=no_context)
-            
+            end_time = time.time()
+            timings["prepare_messages"] += end_time - start_time
             if len(tokenIdsDict["input_ids"]) == 0:
                 continue
 
@@ -59,14 +64,20 @@ class MessageEmbedder:
             encSelectedLayers = self.embedding_model.generate_transformer_embeddings(tokenIdsDict)
             if encSelectedLayers is None:
                 continue
-
             # Aggregate embeddings at the message level
+            start_time = time.time()
             msg_reps, msgIds_new, cfIds_new = self.embedding_model.message_aggregate(encSelectedLayers, msgId_seq, cfId_seq)
+            end_time = time.time()
+            timings["message_aggregate"] += end_time - start_time
+            start_time = time.time()
             msg_reps = np.stack(msg_reps)
             msg_reps = np.squeeze(msg_reps, axis=1)
             pooled_embeddings = np.transpose(msg_reps, (0, 2, 1))
             pooled_embeddings = pooled_embeddings.tolist()
+            end_time = time.time()
+            timings["embedding_stacking"] += (end_time - start_time)
             all_embeddings.extend(pooled_embeddings)
+        
         
         return all_embeddings
         
--- server/server.py
+++ server/server.py
@@ -10,6 +10,7 @@ from embed import Embedder
 import threading 
 import queue
 import os
+import argparse
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:512,expandable_segments:True"
 
 
@@ -23,31 +24,29 @@ class MyFlightServer(flight.FlightServerBase):
         self.embedded_data = {}
         worker = threading.Thread(target=self._embedding_worker, daemon=True)
         worker.start()
-       
         self.embeddings= None  # Instance variable to store embeddings
         self.embeddingcompletion = False
         self.embedding_total_time = 0
         self.network_time_to_cronus = 0
         self.data_loading_time = 0
         self.network_time = 0
-        self.messages = 0
-        # self.embedder = MessageEmbedder("roberta-large")  
-        self.embedder = Embedder("roberta-large")  
+        self.messages = 0 
 
     def _embedding_worker(self):
 
         while True:
+            self.embedder = MessageEmbedder("roberta-large")  
             batch_id, table = self.incoming_data_queue.get()  # block until new data
             # Check for exit condition if needed (not shown)
             print(f"Worker: Embedding for batch_id={batch_id} started...")
             
             message_id = table.column('message_id').to_pylist()
             text_data = table.column('message').to_pylist()
-            # combined_list = [[mid, text] for mid, text in zip(message_id, text_data)]
+            combined_list = [[mid, text] for mid, text in zip(message_id, text_data)]
             start_embedding_time = time.time()
             print("starting to embed")
-            # result = self.embedder.get_embeddings(combined_list)
-            result = self.embedder.embed(text_data, [-4, -3, -2, -1])
+            result = self.embedder.get_embeddings(combined_list)
+            # result = self.embedder.embed(text_data, [-4, -3, -2, -1])
             end_embedding_time = time.time()
             # self.embeddings = result
             
@@ -99,10 +98,13 @@ class MyFlightServer(flight.FlightServerBase):
         
         return flight.RecordBatchStream()
 
-def start_server():
-    server = MyFlightServer(('0.0.0.0', 5111))  # Listen on all interfaces
-    print("Starting server on port 5111")
+def start_server(port):
+    server = MyFlightServer(('0.0.0.0', port))  # Listen on all interfaces
+    print("Starting server on port ", port)
     server.serve()
 
 if __name__ == "__main__":
-    start_server()
+    parser = argparse.ArgumentParser(description="Start MyFlightServer with a specific host and port.")
+    parser.add_argument("--port", type=int, help="Port to bind the server (default: 5111)")
+    args = parser.parse_args()
+    start_server(args.port)
